fb2a0b7aad7a54989e8efdbc9853ed7904366e44
Sinem BALITATLI <95022034+sinembalitatli@users.noreply.github.com>
2022-12-19 23:30:17 +0300

    Update DistributionCountingSort.py

--- DistributionCountingSort.py
+++ DistributionCountingSort.py
@@ -2,9 +2,15 @@
 #Sorts an array of integers from a limited range by distribution counting
 #Input: An array A[0..n − 1] of integers between l and u (l ≤ u)
 #Output: Array S[0..n − 1] of A’s elements sorted in nondecreasing order
-def DistributionCountingSort(A,n,D,S):
+def DistributionCountingSort(A,n):
+    D = list()
+    S = list()  # önceden boyutunu bilinen  boş listeler oluşturmak  için çözüm olabilir
+    for j in range(1, n + 1):
+        D.append(0)
+    for a in range(1, n + 1):
+        S.append(0)
 
-    #önceden eleman sayısı bilinen boş array oluşturma???
+
     lmin=min(A)
     umax=max(A)
     x=(umax-lmin)+1
@@ -29,9 +35,8 @@ def DistributionCountingSort(A,n,D,S):
     print(S)
 A=[13,11,12,13,12,12]
 n=len(A)
-D=[0,0,0,0,0,0]#iyileştirme yaparsak:frekansa bağlı boyutu değişiyor frekansını bulup  boyut ona bağlı oluşturma,yada direk eleman sayısı kadar oluşturursa garanti çalışır sadece bazı durumlarda gereksiz alan kullanımı
-S=[0,0,0,0,0,0]
-DistributionCountingSort(A,n,D,S)
+
+DistributionCountingSort(A,n)
 
 
 
